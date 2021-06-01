d8be99b705f6a3dae183c625905f678baedbbd20
Plaksin Aleksander <plaksin93ao@gmail.com>
2021-06-01 10:46:45 +0300

    distinguish game object in separate class

--- include/gameobject.cpp
+++ include/gameobject.cpp
@@ -1,6 +1,40 @@
 #include "gameobject.h"
 
+#include "texturemanager.h"
+
 GameObject::GameObject()
 {
+}
 
+void GameObject::load(int x, int y, int width, int height, std::string texture_id)
+{
+    x_ = x;
+    y_ = y;
+    width_ = width;
+    height_ = height;
+    id_ = texture_id;
+
+    currentRow_ = 1;
+    currentFrame_ = 1;
+}
+
+
+void GameObject::draw(SDL_Renderer *renderer)
+{
+    TextureManager::instance()->drawFrame(id_, x_, y_,
+                                          width_, height_,
+                                          currentRow_, currentFrame_, renderer);
+    std::cout << "game object draw" << std::endl;
+}
+
+void GameObject::update()
+{
+    if (x_ < 500)
+        ++x_;
+    std::cout << "game object update" << std::endl;
+}
+
+void GameObject::clean()
+{
+    std::cout << "game object clean" << std::endl;
 }
--- include/gameobject.h
+++ include/gameobject.h
@@ -1,17 +1,24 @@
 #ifndef GAMEOBJECT_H
 #define GAMEOBJECT_H
 
+#include <SDL2/SDL.h>
+
+#include <iostream>
 
 class GameObject
 {
 public:
     GameObject();
-    void draw();
-    void update();
-    void clean();
+    void load(int x, int y, int width, int height, std::string texture_id);
+    void draw(SDL_Renderer* renderer);;
+    void update();;
+    void clean();;
 
 protected:
+    std::string id_;
+    int currentFrame_;
+    int currentRow_;
     int x_, y_;
+    int width_, height_;
 };
-
 #endif // GAMEOBJECT_H
