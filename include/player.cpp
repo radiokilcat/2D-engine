86acac484736ce76107ac938fba9b23275329522
Plaksin Aleksander <plaksin93ao@gmail.com>
2021-06-01 10:47:24 +0300

    inherit game object in player class

--- include/player.cpp
+++ include/player.cpp
@@ -4,3 +4,23 @@ Player::Player()
 {
 
 }
+
+void Player::load(int x, int y, int width, int height, std::string texture_id)
+{
+    GameObject::load(x, y, width, height, texture_id);
+}
+
+void Player::draw(SDL_Renderer* renderer)
+{
+    GameObject::draw(renderer);
+}
+
+void Player::update()
+{
+    --x_;
+}
+
+void Player::clean()
+{
+    GameObject::clean();
+}
--- include/player.h
+++ include/player.h
@@ -1,11 +1,17 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include "gameobject.h"
 
-class Player
+class Player: public GameObject
 {
 public:
     Player();
+    void load(int x, int y, int width, int height, std::string texture_id);
+    void draw(SDL_Renderer* renderer);
+    void update();
+    void clean();
+private:
 };
 
 #endif // PLAYER_H
