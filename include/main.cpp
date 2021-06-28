713a7de9941176f5e206efda88d4f49a5c6a1e75
Plaksin Aleksander <plaksin93ao@gmail.com>
2021-06-28 22:24:07 +0300

    Make fixed fps

--- include/main.cpp
+++ include/main.cpp
@@ -6,16 +6,28 @@
 #include "cleanup.h"
 #include "game.h"
 
+const int FPS = 60;
+const int DELAY_TIME = 1000.0f / FPS;
+
 int main()
 {
+    uint32_t frameStart, frameTime;
     if(Game::instance()->init("game", 100, 100, 640, 480, false))
     {
         while(Game::instance()->running())
         {
+            frameStart = SDL_GetTicks();
+
             Game::instance()->handleEvents();
             Game::instance()->update();
             Game::instance()->render();
-            SDL_Delay(10);
+
+            frameTime = SDL_GetTicks() - frameStart;
+
+            if (frameTime < DELAY_TIME)
+            {
+                SDL_Delay((int)(DELAY_TIME - frameTime));
+            }
         }
         Game::instance()->clean();
     }
